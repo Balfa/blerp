73f5cccb069dfa63c783928ab10f748d2d4ecee2
Mo Cassidy <mcassidy@hchb.com>
2022-12-02 07:54:41 -0600

    dotnet_new_this_app.sh

---
+++ dotnet_new_this_app.sh
@@ -0,0 +1,12 @@
+# Below is the command used to scaffold this app
+dotnet new nuget \
+--force \
+-t Blerp \
+-au mo \
+-ta "just;random;crap" \
+-c "https://github.com/Balfa" \
+-sc GitHub \
+-gu Balfa \
+-gp Blerp \
+-am AllEnabledByDefault \
+-tr true
